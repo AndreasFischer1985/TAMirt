17aab9a2b0d8d748851e1a411595efabc70ee093
Dr. Andreas Fischer <45405681+AndreasFischer1985@users.noreply.github.com>
2019-03-22 21:35:28 +0100

    Add files via upload

--- R/checkItemQuality.r
+++ R/checkItemQuality.r
@@ -4,23 +4,30 @@
 #' @export
 
 
-checkItemQuality <- function (data, deleted = NULL) 
+checkItemQuality <- function (data, exclude = NULL, cutoffs = c(0.2, 2, 2, 0.3), 
+    reduce = T) 
 {
+    library(TAM)
     names = seq(1:dim(data)[2])
-    if (length(deleted) > 0) 
-        names = names[-deleted]
-    if (length(deleted) > 0) 
-        data = data[, -deleted]
-    ta = tam.mml(data, control = list(progress = F))
-    mf = tam.modelfit(ta, progress = F)
-    iff = tam.fit(ta, progress = F)
-    d = cbind(names, (apply(data.frame(iff$itemfit[["Infit_p"]], 
-        iff$itemfit[["Outfit_p"]]), 1, min)), (apply(data.frame(iff$itemfit[["Infit_pholm"]], 
-        iff$itemfit[["Outfit_pholm"]]), 1, min)), (-1 * IRT.itemfit(ta)$RMSD[, 
-        2]), (-1 * apply(abs(mf$aQ3.matr), 1, mean, na.rm = T)), 
-        (mf$chisquare.itemfit$p.holm), -iff$itemfit[["Infit"]], 
-        -iff$itemfit[["Outfit"]])
-    colnames(d) = c("Nummer", "Fit_p", "Fit_pholm", "-RMSD", 
-        "-MaQ3", "X2_ph", "-infit", "-outfit")
-    return(d)
+    if (length(exclude) > 0) 
+        names = names[-exclude]
+    if (length(exclude) > 0) 
+        data = data[, -exclude]
+    ta = TAM::tam.mml(data, control = list(progress = F))
+    mf = TAM::tam.modelfit(ta, progress = F)
+    iff = TAM::tam.fit(ta, progress = F)
+    d = NULL
+    if (is.null(cutoffs)) 
+        d = cbind((IRT.itemfit(ta)$RMSD[, 2]), iff$itemfit[["Infit"]], 
+            iff$itemfit[["Outfit"]], (1 * apply(abs(mf$aQ3.matr), 
+                1, mean, na.rm = T)))
+    else d = cbind((IRT.itemfit(ta)$RMSD[, 2]) <= cutoffs[1], 
+        iff$itemfit[["Infit"]] <= cutoffs[2], iff$itemfit[["Outfit"]] <= 
+            cutoffs[3], (1 * apply(abs(mf$aQ3.matr), 1, mean, 
+            na.rm = T)) <= cutoffs[4])
+    colnames(d) = c("Infit", "Outfit", "RMSD", "MaQ3")
+    rownames(d) = names
+    if (reduce) 
+        return(rowSums(d) == dim(d)[2])
+    else return(d)
 }
--- R/plotQ3.r
+++ R/plotQ3.r
@@ -4,7 +4,7 @@
 #' @export
 
 
-plotQ3 <- function (model) 
+plotQ3 <- function (model, cutoff = 0.3) 
 {
     palette <- colorRampPalette(c(rgb(1, 1, 1), rgb(0/255, 84/255, 
         122/255)))(256)
@@ -17,7 +17,7 @@ plotQ3 <- function (model)
     s = seq(0, 1, length.out = dim(aQ3)[2])
     for (i1 in 1:dim(aQ3)[2]) for (i2 in 1:dim(aQ3)[2]) text(x = s[i1], 
         y = s[i2], round(aQ3[i1, i2], 2), cex = 0.5, col = c(rgb(0, 
-            0, 0, 0), "red")[(abs(aQ3[i1, i2]) > 0.2) + 1])
+            0, 0, 0), "red")[(abs(aQ3[i1, i2]) > cutoff) + 1])
     legend(0.8, 1.15, c("0", "1"), fill = palette[c(1, length(palette))], 
         xpd = NA)
     title("adjusted Q3-matrix")
